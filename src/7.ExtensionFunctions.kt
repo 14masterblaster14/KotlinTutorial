8156dbdb9d3f52f167541ddce6a3518ee8c43137
MasterBlaster <14masterblaster14@gmail.com>
2019-07-11 19:13:39 +0530

    Refinement

--- src/7.ExtensionFunctions.kt
+++ src/7.ExtensionFunctions.kt
@@ -1,20 +1,22 @@
+// Extension Functions
+
 fun main(args: Array<String>) {
 
     var student = Student()
-    println("Pass Status " + student.hasPassed(60))
-    println("Scholarship Status " + student.isScholar(95))
+    println("Pass Status " + student.hasPassed(60))         //  Pass Status true
+    println("Scholarship Status " + student.isScholar(95))  //  Scholarship Status true
 
     //We can also create the extension function for standard/ predefined classes as well
     val string1 = "Hello"
     val string2 = "World!"
     var string3 = "Hey,"
 
-    println(string3.add(string1, string2))
+    println(string3.add(string1, string2))  // Hey,HelloWorld!
 
     val x = 6
     val y = 10
     val greaterValue = x.greaterValue(y)
-    println(greaterValue)
+    println(greaterValue)           // 10
 }
 
 class Student {
