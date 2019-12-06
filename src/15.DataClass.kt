a88ca6a5d9a449d9dded81ea1ba744492807c8b7
MasterBlaster <14masterblaster14@gmail.com>
2019-12-06 21:15:07 +0530

    Refinement

--- src/15.DataClass.kt
+++ src/15.DataClass.kt
@@ -10,18 +10,18 @@ fun main(args: Array<String>) {
     var user2 = User("Master", 7)
 
     if (user1 == user2) {
-        println("objects are equal")    // executed
+        println("objects are equal")    // objects are equal
     } else {
         println("objects are not equal")
     }
 
-    println(user1.toString())
+    println(user1.toString())   //  User(name=Master, id=7)
 
-    var user3 = user1.copy("Blaster")   // user1.copy(name = "Blaster")
-    println(user3)
+    var user3 = user1.copy("Blaster")  // user1.copy(name = "Blaster")
+    println(user3)              //  User(name=Blaster, id=7)
 
-    var user4 = user1.copy(id = 10)   // user1.copy(10)
-    println(user4)
+    var user4 = user1.copy(id = 10)         // user1.copy(10)
+    println(user4)              //  User(name=Master, id=10)
 
 }
 
