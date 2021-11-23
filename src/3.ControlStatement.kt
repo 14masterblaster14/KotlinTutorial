7a23373729db851afa61f553aa58f66a1c72abfb
14masterblaster14 <14masterblaster14@gmail.com>
2021-11-23 17:39:27 +0530

    Update 3.ControlStatement.kt

--- src/3.ControlStatement.kt
+++ src/3.ControlStatement.kt
@@ -102,11 +102,11 @@ fun main(args: Array<String>) {
     // Elvis operator
 
     var name = "MasterBlaster"
-    var name1 = ""
+    var name1 = null
 
     val name_length = name.length
     val name1_length = name1.length
-    println(name_length)
-    println(name1_length)
-
+    
+    println(name1?.length:name.length) // 12
+    
 }
