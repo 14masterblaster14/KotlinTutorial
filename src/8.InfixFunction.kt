672ddde699e72b9f414f234d2e5ca5ed7a87549e
14masterblaster14 <14masterblaster14@gmail.com>
2021-11-24 09:23:12 +0530

    Update 8.InfixFunction.kt

--- src/8.InfixFunction.kt
+++ src/8.InfixFunction.kt
@@ -1,17 +1,30 @@
 // Infix Function ::
 //      - It can be member function or extension function
 //      - They have a just a single parameter
+//      - The parameter must not accept variable number of arguments and must have no default value
 //      - They have prefix of "infix"
 //      - All infix functions are Extension functions but all extension functions are not Infix.
+
+//  Standard library infix function notation : e.g Bitwise operator e.g and , ++ , -- etc
+//  User defined infix function notation
 
 fun main(args: Array<String>) {
 
     val x = 6
     val y = 10
     //val greaterValue = x.greaterValue1(y) //  extension function
+    // val greaterValue = 6 greaterValue1 10 // infix function
     val greaterValue = x greaterValue1 y // infix function
     println(greaterValue)               // 10
+    
+    val m = math()
+    // call using infix notation
+    val result = m square 3     // infix function
+    print("The square of a number is: "+result) // The square of a number is: 9
+    
 }
+
+// user defined infix member function
 
 infix fun Int.greaterValue1(other: Int): Int {
 
@@ -19,4 +32,12 @@ infix fun Int.greaterValue1(other: Int): Int {
         return this
     else
         return other
+}
+
+class math {
+    // user defined infix member function
+    infix fun square(n : Int): Int{
+        val num = n * n
+        return num
+    }
 }
